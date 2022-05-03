8259aed31269908beffbab12182be78687ecfc33
furkantok <furkant.ftk@gmail.com>
2022-05-04 02:10:18 +0300

    BasicNavigations

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -0,0 +1,67 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //1- Set up the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //2- Create instance of the Selenium WebDriver
+        // This is the line opening an empty browser
+        WebDriver driver = new ChromeDriver();
+
+        //3- Go to "https://www.tesla.com"
+        driver.get("https://www.tesla.com");
+
+        //System.out.println("driver.getTitle() = " + driver.getTitle());
+        String currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        // get the current URL using Selenium
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
+
+        //stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate back
+        driver.navigate().back();
+
+        //stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate forward
+        driver.navigate().forward();
+
+        //stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+        //stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use navigate().to()
+        driver.navigate().to("https://www.google.com");
+
+        currentTitle = driver.getTitle();
+
+        //get the title of the page
+        System.out.println("currentTitle = " + currentTitle);
+
+        // get the current URL using Selenium
+        currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL = " + currentURL);
+
+
+
+
+    }
+
+}
--- src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -10,6 +10,7 @@ public class SeleniumTest {
     public static void main(String[] args) {
 
         //1- Setting up the web driver manager
+        //We create our "Browser Driver"
         WebDriverManager.chromedriver().setup();
 
         //2- Create instance of the chrome driver
