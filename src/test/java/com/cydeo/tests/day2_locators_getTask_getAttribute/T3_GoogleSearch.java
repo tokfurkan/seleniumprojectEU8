3f8f769163b4ee90751c768717e887e7e655a383
furkantok <furkant.ftk@gmail.com>
2022-05-06 23:06:44 +0300

    day2_T3_GoogleSearch task

--- src/test/java/com/cydeo/tests/day2_locators_getTask_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getTask_getAttribute/T3_GoogleSearch.java
@@ -1,8 +1,46 @@
 package com.cydeo.tests.day2_locators_getTask_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T3_GoogleSearch {
 
     public static void main(String[] args) {
+
+        //TC#3: Google Search
+        //1. Open a Chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to: https://www.google.com
+
+        driver.get("https://www.google.com");
+
+        //3. Write "apple" in seach box
+        //4. Press ENTER using Keys.ENTER
+
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple" + Keys.ENTER);
+
+        //5. Verify title: Expected: Title should start with "apple" keyword
+
+        String expectedInTitle = "apple";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.startsWith(expectedInTitle)){
+            System.out.println("Title verification PASSED!");
+        } else {
+            System.out.println("Title verification FAILED!");
+        }
+
+
+
 
 
 
