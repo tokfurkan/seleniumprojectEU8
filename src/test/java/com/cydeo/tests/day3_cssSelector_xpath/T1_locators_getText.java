60ae48ea87ba214ce01483e5b770e734c10e6381
furkantok <furkant.ftk@gmail.com>
2022-05-12 22:41:16 +0300

    day3_T1_locators_getText task

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -0,0 +1,55 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_locators_getText {
+
+    public static void main(String[] args) {
+
+        //TC#1 NextBaseCRM, locators and getText() practice
+        //1. Open a Chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+
+        //2. Go to: https://login1.nextbasecrm.com/
+
+        driver.get("https://login.nextbasecrm.com/");
+
+        //3. Enter incorrect username: "incorrect"
+
+        WebElement inputUsername = driver.findElement(By.className("login-inp"));
+        inputUsername.sendKeys("incorrect");
+
+        //4. Enter incorrect password: "incorrect"
+
+        WebElement inputPassword = driver.findElement(By.name("USER_PASSWORD"));
+        inputPassword.sendKeys("incorrect");
+
+        //5. Click to login button
+
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        //6. Verify error message text is as expected: Incorrect login or password
+
+        WebElement errorMessage = driver.findElement(By.className("errortext"));
+
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = errorMessage.getText();
+
+        if (expectedErrorMessage.equals(actualErrorMessage)){
+            System.out.println("Error message verification passed");
+        }else {
+            System.out.println("Error message verification failed");
+        }
+
+
+    }
+
+}
