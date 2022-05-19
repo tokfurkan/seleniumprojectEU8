59e589dee755f52712dbe093fb95eb6f7986a18b
furkantok <furkant.ftk@gmail.com>
2022-05-20 00:53:03 +0300

    day5_TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,10 +1,19 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("BeforeClass is running!");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("AfterClass is running");
+    }
 
     @BeforeMethod
     public void setUpMethod(){ //it will get executed before each @Test
@@ -19,12 +28,32 @@ public class TestNG_Intro {
     @Test
     public void test1(){
         System.out.println("Test 1 is running...");
+
+        //assert.equals --> compares 2 of the same things
+        String actual = "apple";
+        String expected = "apple";
+        Assert.assertEquals(actual, expected);
+
     }
 
     @Test
     public void test2(){
         System.out.println("Test 2 is running...");
+
+        //assert.true --> accepts Boolean as parameter, returns Boolean again
+        String actual = "apple";
+        String expected = "apple";
+        Assert.assertTrue(actual.equals(expected));
+
     }
+
+    //@BeforeClass will get executed only ONCE before each method
+    //@BeforeMethod will get executed several times (before tests)
+    //@AfterMethod will get executed after each test
+    //@AfterClass will get executed after all methods got executed, it will run as final
+
+
+
 
 
 }
