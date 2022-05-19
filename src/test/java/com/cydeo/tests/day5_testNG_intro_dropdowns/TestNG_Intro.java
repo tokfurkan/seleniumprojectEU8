fb9148de3ead1f0b1bf7822f62010b57dc0bc00b
furkantok <furkant.ftk@gmail.com>
2022-05-19 23:20:29 +0300

    day5_TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,12 +1,30 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
+
+    @BeforeMethod
+    public void setUpMethod(){ //it will get executed before each @Test
+        System.out.println("BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){ //it will get executed after each @Test
+        System.out.println("AfterMethod is running!");
+    }
 
     @Test
     public void test1(){
         System.out.println("Test 1 is running...");
     }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running...");
+    }
+
 
 }
