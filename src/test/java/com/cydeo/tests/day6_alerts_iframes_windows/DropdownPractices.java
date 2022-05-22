122af1796e8ae6c38cf5acb2df47af431254ab1b
furkantok <furkant.ftk@gmail.com>
2022-05-23 02:27:11 +0300

    day6_DropdownPractices

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -91,6 +92,11 @@ public class DropdownPractices {
         Assert.assertEquals(expectedDay, actualDay);
 
 
+    }
+
+    @AfterMethod
+    public void teardown(){
+        driver.close();
     }
 
 
