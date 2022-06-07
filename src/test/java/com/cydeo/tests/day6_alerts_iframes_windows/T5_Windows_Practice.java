68d7dbb44baa67ca0ed360d9a5b9940ebb8c4684
furkantok <furkant.ftk@gmail.com>
2022-06-07 17:01:43 +0300

    day6_T5_Windows_Practice

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -70,6 +71,11 @@ public class T5_Windows_Practice {
         System.out.println("Title after click : " + actualTitle);
 
 
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 
 
